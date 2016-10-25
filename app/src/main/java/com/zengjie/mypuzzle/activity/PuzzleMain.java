50949bcde9e723cc1e48a364f4922a55efecae7d
zengjie <zj602497556@qq.com>
2016-10-25 17:50:55 +0800

    MyPuzzle开发：第二阶段.

--- app/src/main/java/com/zengjie/mypuzzle/activity/PuzzleMain.java
+++ app/src/main/java/com/zengjie/mypuzzle/activity/PuzzleMain.java
@@ -1,15 +1,35 @@
 package com.zengjie.mypuzzle.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.zengjie.mypuzzle.R;
 
 public class PuzzleMain extends Activity {
 
+    //拼图完成时显示的最后一个图片
+    public static Bitmap mLastBitmap;
+    //设置为N*N显示
+    public static int TYPE = 2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_main);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
